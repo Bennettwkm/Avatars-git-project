a4252e683258b1b3fab022274fcf8adaf576a715
Bennett WKM <bennettwkm@gmail.com>
2024-07-07 17:32:55 -0500

    Added Testing Class

---
+++ src/test/java/org/example/Testing.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import sun.jvm.hotspot.opto.MachIfNode;
+
+public class Testing {
+
+    public static void main(String[] args) {
+        System.out.println("My third git class");
+
+
+    }
+}
