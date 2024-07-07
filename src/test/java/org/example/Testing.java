3ac7fee4af2657632b94c280a03a9c2ea212e54d
Bennett WKM <bennettwkm@gmail.com>
2024-07-07 17:43:22 -0500

    My third push

--- src/test/java/org/example/Testing.java
+++ src/test/java/org/example/Testing.java
@@ -1,11 +1,14 @@
 package org.example;
 
+import jdk.internal.org.jline.terminal.TerminalBuilder;
 import sun.jvm.hotspot.opto.MachIfNode;
 
 public class Testing {
 
     public static void main(String[] args) {
         System.out.println("My third git class");
+        System.out.println("I am having the time of my life");
+        System.out.println("I cannot wait to get a job");
 
 
     }
